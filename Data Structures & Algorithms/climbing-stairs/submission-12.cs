dcdcd34c7fae834770d1560efbefd2fea3b32ff7
Alexandru Velicu <40795126+avomnibit@users.noreply.github.com>
2026-07-06 16:17:22 +0300

    Add: climbing-stairs - submission-12

---
+++ Data Structures & Algorithms/climbing-stairs/submission-12.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if(n<2) return n;
+        int[] cache = new int[n+1];
+        cache[1] =1;
+        cache[2] =2;
+        for(int i=3;i<=n;i++){
+            cache[i] = cache[i-1]+cache[i-2];
+        }
+        return cache[n];
+    }
+}
