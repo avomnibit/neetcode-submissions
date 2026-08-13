60d3a0906fb48d3188037215d200999138278fa5
Alexandru Velicu <40795126+avomnibit@users.noreply.github.com>
2026-08-13 17:56:08 +0300

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int LastStoneWeight(int[] stones) {
+        PriorityQueue<int, int> pq = new PriorityQueue<int,int>();
+        for(int i=0;i<stones.Length;i++){
+            pq.Enqueue(stones[i],-stones[i]);
+        }
+        int e1,e2; 
+        while(pq.Count>0){
+            if(pq.Count==1) return pq.Dequeue();
+            e1 = pq.Dequeue();
+            e2 = pq.Dequeue();
+            if(e1!=e2) { 
+                pq.Enqueue(Math.Abs(e1-e2),-Math.Abs(e1-e2));
+            }
+        }
+        return 0;
+    }
+}
