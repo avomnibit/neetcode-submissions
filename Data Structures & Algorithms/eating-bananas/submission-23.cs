8a8747eb93768fdff2f0a12b1f582b14ae9d7575
Alexandru Velicu <40795126+avomnibit@users.noreply.github.com>
2026-08-14 14:27:39 +0300

    Add: eating-bananas - submission-23

---
+++ Data Structures & Algorithms/eating-bananas/submission-23.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        int i,max=0;
+        for(i=0;i<piles.Length;i++){
+            if(max<piles[i]) max=piles[i];
+        }
+        int l=1,r=max, mid, lmin=0;
+        long minh;
+        while(l<=r && r!=0) {
+            mid=l+(r-l)/2;
+            minh=0;
+            for(i=0;i<piles.Length;i++){
+                minh+=(long)(piles[i]+mid-1)/mid;
+            }
+            if(minh<=h) {
+                lmin = mid;
+                r=mid-1;
+                continue;
+            }
+            l=mid+1;
+        }
+        return lmin;
+    }
+}
